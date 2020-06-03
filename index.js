370670da16f0d05ac4a9394a1bb42cb8fa3262ea
gnehs <jayuiop@gmail.com>
2020-06-03 19:55:16 +0800

    分到五個資料夾裡

--- index.js
+++ index.js
@@ -11,20 +11,47 @@ async function main() {
         res.push({ id, pinyin, verified: verified == 'v', startTime, endTime })
     }
     console.log('csv 檔案解析完成')
+    //create folder
+    function rmdir(d) {
+        var self = arguments.callee
+        if (fs.existsSync(d)) {
+            fs.readdirSync(d).forEach(function (file) {
+                var C = d + '/' + file
+                if (fs.statSync(C).isDirectory()) self(C)
+                else fs.unlinkSync(C)
+            })
+            fs.rmdirSync(d)
+        }
+    }
+    rmdir('./result');
+    for (let folder of ['./result',]) {
+        fs.mkdirSync(folder);
+        for (let folder2 of ['/mp3/', '/mp4/']) {
+            fs.mkdirSync(folder + folder2);
+            for (let folder3 of [1, 2, 3, 4, 5]) {
+                fs.mkdirSync(folder + folder2 + folder3);
+            }
+        }
+    }
     console.log('ffmpeg 處理中...')
+    let counter
+    counter = 0
     for (let { pinyin, startTime, endTime } of res) {
+        counter++
         ffmpeg('./original/howhow.mp3')
             .setStartTime(startTime)
             .setDuration(endTime - startTime)
-            .output(`./result/mp3/${pinyin}.mp3`)
+            .output(`./result/mp3/${counter % 5 + 1}/${pinyin}.mp3`)
             .on('error', err => console.log('error: ', pinyin, err))
             .run();
     }
+    counter = 0
     for (let { pinyin, startTime, endTime } of res) {
+        counter++
         ffmpeg('./original/howhow.mp4')
             .setStartTime(startTime)
             .setDuration(endTime - startTime)
-            .output(`./result/mp4/${pinyin}.mp4`)
+            .output(`./result/mp4/${counter % 5 + 1}/${pinyin}.mp4`)
             .on('error', err => console.log('error: ', pinyin, err))
             .run();
     }
