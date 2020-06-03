88a7029c3ec8b65692e6e4629a8bfe100e195f32
gnehs <jayuiop@gmail.com>
2020-06-03 15:08:54 +0800

    parser done

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ const getStream = require('get-stream');
 const readCSVData = async filePath => (await getStream.array(fs.createReadStream(filePath).pipe(parse({ delimiter: ',' }))))
 
 async function main() {
-    let res = await readCSVData('./original/HowHow 發音標註眾包 - 發音表.csv')
-    console.log(res)
+    let res = []
+    for (let [id, pinyin, verified, startTime, endTime] of (await readCSVData('./original/HowHow 發音標註眾包 - 發音表.csv'))) {
+        id = Number(id)
+        res.push({ id, pinyin, verified: verified == 'v', startTime, endTime })
+    }
+    console.log('csv 檔案解析完成')
+    console.log('ffmpeg 處理中...')
+    for (let { pinyin, startTime, endTime } of res) {
+        ffmpeg('./original/howhow.mp3')
+            .setStartTime(startTime)
+            .setDuration(endTime - startTime)
+            .output(`./result/mp3/${pinyin}.mp3`)
+            .on('error', err => console.log('error: ', pinyin, err))
+            .run();
+    }
+    for (let { pinyin, startTime, endTime } of res) {
+        ffmpeg('./original/howhow.mp4')
+            .setStartTime(startTime)
+            .setDuration(endTime - startTime)
+            .output(`./result/mp4/${pinyin}.mp4`)
+            .on('error', err => console.log('error: ', pinyin, err))
+            .run();
+    }
 }
 main() 
